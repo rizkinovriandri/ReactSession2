ceeeac7787482150b0303b64169fa9d0393c1fe9
rizkinovriandri <qieroway@gmail.com>
2020-08-13 22:18:59 +0700

    Reset after Calculate

--- script.js
+++ script.js
@@ -1,44 +1,56 @@
 $(document).ready(function(){
 
-    var check = true;
+    var finish = false;
+
+    function clearScreen(){document.getElementById("output").value = '';}
 
     $("#btn1").click(function(){
+        if (finish === true ) {clearScreen(); finish = false;}
         document.getElementById("output").value += 1;
     });
 
     $("#btn2").click(function(){
+        if (finish === true ) {clearScreen(); finish = false;}
         document.getElementById("output").value += 2;
     });
 
     $("#btn3").click(function(){
+        if (finish === true ) {clearScreen(); finish = false;}
         document.getElementById("output").value += 3;
     });
 
     $("#btn4").click(function(){
+        if (finish === true ) {clearScreen(); finish = false;}
         document.getElementById("output").value += 4;
     });
 
     $("#btn5").click(function(){
+        if (finish === true ) {clearScreen(); finish = false;}
         document.getElementById("output").value += 5;
     });
 
     $("#btn6").click(function(){
+        if (finish === true ) {clearScreen(); finish = false;}
         document.getElementById("output").value += 6;
     });
 
     $("#btn7").click(function(){
+        if (finish === true ) {clearScreen(); finish = false;}
         document.getElementById("output").value += 7;
     });
 
     $("#btn8").click(function(){
+        if (finish === true ) {clearScreen(); finish = false;}
         document.getElementById("output").value += 8;
     });
 
     $("#btn9").click(function(){
+        if (finish === true ) {clearScreen(); finish = false;}
         document.getElementById("output").value += 9;
     });
 
     $("#btn0").click(function(){
+        if (finish === true ) {clearScreen(); finish = false;}
         document.getElementById("output").value += 0;
     });
 
@@ -68,6 +80,7 @@ $(document).ready(function(){
 
     $("#btnCalc").click(function(){
         document.getElementById("output").value = eval(document.getElementById("output").value);
+        finish = true;
     });
 
 });
